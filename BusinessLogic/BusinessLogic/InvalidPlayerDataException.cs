29cd93439ca0adbcdf0c6cdf6f283251e028236f
Facundo Arancet <facuarancet97@gmail.com>
2021-04-07 09:48:15 -0300

    Added exception for invalid player data

---
+++ BusinessLogic/BusinessLogic/InvalidPlayerDataException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+	public class InvalidPlayerDataException : Exception
+	{
+		public InvalidPlayerDataException(string message) : base(message)
+		{
+		}
+
+		public InvalidPlayerDataException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+	}
+}
