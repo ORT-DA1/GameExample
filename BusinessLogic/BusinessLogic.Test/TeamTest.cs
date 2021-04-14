44262ae07afbb42f6bcb6b1559af0e09dfa828d4
Facundo Arancet <facuarancet97@gmail.com>
2021-04-14 09:28:31 -0300

    Team implementation

--- BusinessLogic/BusinessLogic.Test/TeamTest.cs
+++ BusinessLogic/BusinessLogic.Test/TeamTest.cs
@@ -7,8 +7,45 @@ namespace BusinessLogic.Test
 	public class TeamTest
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void NewTeamTest()
 		{
+			var newTeam = new Team("myteam");
+			Assert.IsNotNull(newTeam);
 		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void NameNotNullTest()
+		{
+			new Team(null);
+		}
+
+		[TestMethod]
+		public void GetTeamNameTest()
+		{
+			var teamName = "teamName";
+			var team = new Team(teamName);
+			Assert.AreEqual(teamName, team.Name);
+		}
+
+		[TestMethod]
+		public void EqualTeamTest()
+		{
+			var teamName = "teamName";
+			var team1 = new Team(teamName);
+			var team2 = new Team(teamName);
+			Assert.AreEqual(team1, team2);
+		}
+
+		[TestMethod]
+		public void NotEqualTeamTest()
+		{
+			var teamName1 = "teamName1";
+			var teamName2 = "teamName2";
+			var team1 = new Team(teamName1);
+			var team2 = new Team(teamName2);
+			Assert.AreNotEqual(team1, team2);
+		}
+
 	}
 }
---
+++ BusinessLogic/BusinessLogic/Team.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+	public class Team
+	{
+		public Team(string name)
+		{
+			Name = name;
+		}
+
+		public string Name { get => name ; private set => SetName(value); }
+
+		private string name;
+		private void SetName(string aName)
+		{
+			if (aName == null)
+				throw new ArgumentNullException("El equipo debe tener un nombre");
+
+			name = aName;
+			
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj == null || this.GetType() != obj.GetType())
+			{
+				return false;
+			}
+
+			var team = (Team)obj;
+			return team.Name == this.Name;
+
+		}
+
+	}
+}
